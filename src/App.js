08cbe142d59b3c533ea43acdf2a4f4a682a5ba73
Adesh Lad <ladadesh@gmail.com>
2024-03-14 20:02:19 +0530

    code added for gh pages.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-
-import './App.css';
-import Header from './components/Header';
-import {BrowserRouter as Router , Route, Routes} from "react-router-dom"
+import "./App.css";
+import Header from "./components/Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
-import Emenu from './components/Emenu';
-import StoreLogin from './components/StoreLogin';
-import ApplyStore from './components/ApplyStore';
-import QRCode from './components/QRCode';
-import CssBaseline from '@mui/material/CssBaseline';
+import Emenu from "./components/Emenu";
+import StoreLogin from "./components/StoreLogin";
+import ApplyStore from "./components/ApplyStore";
+import QRCode from "./components/QRCode";
+import CssBaseline from "@mui/material/CssBaseline";
 
 function App() {
   return (
     <>
       <CssBaseline />
       <Router>
-      <Header />
+        <Header />
         <Routes>
-          <Route path='/' exact={true} Component={Home} />  
+          <Route path="/eway-clone" exact={true} Component={Home} />
           <Route path="/qr" Component={QRCode} />
           <Route path="/list" Component={Emenu} />
           <Route path="/login" Component={StoreLogin} />
           <Route path="/apply" Component={ApplyStore} />
         </Routes>
       </Router>
-      </>
+    </>
   );
 }
 
